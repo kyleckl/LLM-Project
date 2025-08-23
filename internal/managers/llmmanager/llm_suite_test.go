9afaf16aa3a9308de009bb4012ada3b9e144511f
Kyle Lam <kyleclam.04@gmail.com>
2025-08-22 18:48:00 -0700

    feat: added print streaming to the LLM http response

--- internal/managers/llmmanager/llm_suite_test.go
+++ internal/managers/llmmanager/llm_suite_test.go
@@ -2,8 +2,6 @@ package llmmanager_test
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"llmApp/internal/managers/llmmanager"
 	"testing"
 
@@ -17,25 +15,36 @@ func TestAdapters(t *testing.T) {
 }
 
 var _ = Describe("HTTPClient", func() {
-	It("Should be able to send a query", func() {
-		ctx := context.Background()
-		llm := llmmanager.NewLLMClient("ollamaModel", "")
+	ctx := context.Background()
+	llm := llmmanager.NewLLMClient("ollamaModel", "")
+
+	It("Should be able to send a request to the LLM", func() {
+		// Create a prompt, enabling streaming responses
+		// Stream is set to true because we don't want to wait for the entire response
 		prompt := llmmanager.QueryAdapter{
 			Model:  "gemma3:1b",
 			Prompt: "How has redlining in the United States affected urban development in recent years?",
-			Stream: false,
+			Stream: true,
 		}
 
 		resp, err := llm.SendQuery(ctx, prompt)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(resp.StatusCode).To(Equal(200))
+	})
 
-		defer resp.Body.Close()
-		var response llmmanager.ResponseAdapter
-		err = json.NewDecoder(resp.Body).Decode(&response)
+	It("Should be able to stream a response from the LLM", func() {
+		prompt := llmmanager.QueryAdapter{
+			Model:  "gemma3:1b",
+			Prompt: "Explain the theory of relativity in simple terms.",
+			Stream: true,
+		}
+
+		resp, err := llm.SendQuery(ctx, prompt)
 		Expect(err).ToNot(HaveOccurred())
-		Expect(response.Response).ToNot(BeEmpty())
 
-		fmt.Printf("Response: %s", response.Response)
+		// Use the response body as a parameter to stream the response
+		outputString, err := llm.StreamResponse(ctx, resp)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(outputString).ToNot(BeEmpty())
 	})
 })
--- internal/managers/llmmanager/llmmanager.go
+++ internal/managers/llmmanager/llmmanager.go
@@ -1,7 +1,10 @@
 package llmmanager
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
+	"fmt"
 	"llmApp/internal/managers/httpManager"
 	"net/http"
 )
@@ -55,4 +58,30 @@ func (l *llmManager) SendQuery(ctx context.Context, prompt any) (*http.Response,
 	}
 
 	return resp, nil
+}
+
+// Given an HTTP response from the LLM, it streams and returns the response as a string
+func (l *llmManager) StreamResponse(ctx context.Context, resp *http.Response) (response string, err error) {
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		var chunk ResponseAdapter
+		err := json.Unmarshal(line, &chunk)
+		if err != nil {
+			return "", err
+		}
+
+		response += chunk.Response
+		fmt.Print(chunk.Response)
+
+		if chunk.Done {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return response, nil
 }
